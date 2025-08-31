db3a208d0ada7437b12b7af6e032eaf661fca72b
Rafał Grodziński <rafal.grodzinski@gmail.com>
2025-08-31 15:50:50 +0900

    Well of course it didn't work, the architecture was wrong

--- shell/build.sh
+++ shell/build.sh
@@ -3,5 +3,8 @@
 SCRIPT_PATH="$(readlink -f "${BASH_SOURCE}")"
 SCRIPT_DIR="$(dirname "${SCRIPT_PATH}")"
 
-brb -v --triple=x86_64-unknown-linux-gnu --function-sections "${SCRIPT_DIR}/main.brc" "${SCRIPT_DIR}/terminal.brc"
+brb -v --triple=i686-unknown-linux-gnu --function-sections "${SCRIPT_DIR}/main.brc" "${SCRIPT_DIR}/terminal.brc"
+if [ $? -ne 0 ]; then
+    exit 1
+fi
 ld.lld -T "${SCRIPT_DIR}/flat_binary.ld" terminal.o main.o -o shell.bin
