d5d9424a68cb8d1b12c6a4ad8f663383dc644934
Rafał Grodziński <rafal.grodzinski@gmail.com>
2025-09-01 14:58:25 +0900

    Fixed interrupt handler

--- kernel/interrupt.asm
+++ kernel/interrupt.asm
@@ -30,28 +30,28 @@ dd idt_protected_mode + ADDRESS_KERNEL
 %endmacro
 
 idt_protected_mode:
-IDT_ENTRY interrupt_handler_00 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; divide error
-IDT_ENTRY interrupt_handler_01 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; debug exception
-IDT_ENTRY interrupt_handler_02 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; nmi interrupt
-IDT_ENTRY interrupt_handler_03 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; breakpoint
-IDT_ENTRY interrupt_handler_04 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; overflow
-IDT_ENTRY interrupt_handler_05 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; bound range
-IDT_ENTRY interrupt_handler_06 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; invalid opcode
-IDT_ENTRY interrupt_handler_07 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; no math coprocessor
-IDT_ENTRY interrupt_handler_08 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; double fault
-IDT_ENTRY interrupt_handler_09 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; coprocessor segment overrun
-IDT_ENTRY interrupt_handler_0a + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; invalid tss
-IDT_ENTRY interrupt_handler_0b + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; segment not present
-IDT_ENTRY interrupt_handler_0c + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; stack-segment fault
-IDT_ENTRY interrupt_handler_0d + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; general protection
-IDT_ENTRY interrupt_handler_0e + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; page fault
+IDT_ENTRY interrupt_handler_00 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x00 divide error
+IDT_ENTRY interrupt_handler_01 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x01 debug exception
+IDT_ENTRY interrupt_handler_02 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x02 nmi interrupt
+IDT_ENTRY interrupt_handler_03 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x03 breakpoint
+IDT_ENTRY interrupt_handler_04 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x04 overflow
+IDT_ENTRY interrupt_handler_05 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x05 bound range
+IDT_ENTRY interrupt_handler_06 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x06 invalid opcode
+IDT_ENTRY interrupt_handler_07 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x07 no math coprocessor
+IDT_ENTRY interrupt_handler_08 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x08 double fault
+IDT_ENTRY interrupt_handler_09 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x09 coprocessor segment overrun
+IDT_ENTRY interrupt_handler_0a + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x0a invalid tss
+IDT_ENTRY interrupt_handler_0b + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x0b segment not present
+IDT_ENTRY interrupt_handler_0c + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x0c stack-segment fault
+IDT_ENTRY interrupt_handler_0d + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x0d general protection
+IDT_ENTRY interrupt_handler_0e + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x0e page fault
 IDT_ENTRY 0, 0 ; 0x0f
-IDT_ENTRY interrupt_handler_10 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; fpu fault
-IDT_ENTRY interrupt_handler_11 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; alignment check
-IDT_ENTRY interrupt_handler_12 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; machine check
-IDT_ENTRY interrupt_handler_13 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; simd exception
-IDT_ENTRY interrupt_handler_14 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; virtualization exception
-IDT_ENTRY interrupt_handler_15 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; control protection exception
+IDT_ENTRY interrupt_handler_10 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x10 fpu fault
+IDT_ENTRY interrupt_handler_11 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x11 alignment check
+IDT_ENTRY interrupt_handler_12 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x12 machine check
+IDT_ENTRY interrupt_handler_13 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x13 simd exception
+IDT_ENTRY interrupt_handler_14 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x14 virtualization exception
+IDT_ENTRY interrupt_handler_15 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x15 control protection exception
 IDT_ENTRY 0, 0 ; 0x16 reserved
 IDT_ENTRY 0, 0 ; 0x17 reserved
 IDT_ENTRY 0, 0 ; 0x18 reserved
@@ -62,23 +62,23 @@ IDT_ENTRY 0, 0 ; 0x1c reserved
 IDT_ENTRY 0, 0 ; 0x1d reserved
 IDT_ENTRY 0, 0 ; 0x1e reserved
 IDT_ENTRY 0, 0 ; 0x1f reserved
-IDT_ENTRY interrupt_handler_20 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; IRQ 0
-IDT_ENTRY interrupt_handler_21 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; IRQ 1
-IDT_ENTRY interrupt_handler_22 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; IRQ 2
-IDT_ENTRY interrupt_handler_23 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; IRQ 3
-IDT_ENTRY interrupt_handler_24 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; IRQ 4
-IDT_ENTRY interrupt_handler_25 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; IRQ 5
-IDT_ENTRY interrupt_handler_26 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; IRQ 6
-IDT_ENTRY interrupt_handler_27 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; IRQ 7
-IDT_ENTRY interrupt_handler_28 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; IRQ 8
-IDT_ENTRY interrupt_handler_29 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; IRQ 9
-IDT_ENTRY interrupt_handler_2a + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; IRQ a
-IDT_ENTRY interrupt_handler_2b + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; IRQ b
-IDT_ENTRY interrupt_handler_2c + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; IRQ c
-IDT_ENTRY interrupt_handler_2d + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; IRQ d
-IDT_ENTRY interrupt_handler_2e + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; IRQ e
-IDT_ENTRY interrupt_handler_2f + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; IRQ f
-IDT_ENTRY interrupt_handler_30 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; SYS
+IDT_ENTRY interrupt_handler_20 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x20 IRQ 0
+IDT_ENTRY interrupt_handler_21 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x21 IRQ 1
+IDT_ENTRY interrupt_handler_22 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x22 IRQ 2
+IDT_ENTRY interrupt_handler_23 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x23 IRQ 3
+IDT_ENTRY interrupt_handler_24 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x24 IRQ 4
+IDT_ENTRY interrupt_handler_25 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x25 IRQ 5
+IDT_ENTRY interrupt_handler_26 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x26 IRQ 6
+IDT_ENTRY interrupt_handler_27 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x27 IRQ 7
+IDT_ENTRY interrupt_handler_28 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x28 IRQ 8
+IDT_ENTRY interrupt_handler_29 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x29 IRQ 9
+IDT_ENTRY interrupt_handler_2a + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x2a IRQ a
+IDT_ENTRY interrupt_handler_2b + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x2b IRQ b
+IDT_ENTRY interrupt_handler_2c + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x2c IRQ c
+IDT_ENTRY interrupt_handler_2d + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x2d IRQ d
+IDT_ENTRY interrupt_handler_2e + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x2e IRQ e
+IDT_ENTRY interrupt_handler_2f + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x2f IRQ f
+IDT_ENTRY interrupt_handler_30 + ADDRESS_KERNEL, GDT_CODE_PROTECTED_MODE ; 0x30 SYS
 idt_protected_mode_end:
 
 ;
@@ -170,248 +170,332 @@ interrupt_init_protected_mode:
 
 ;
 ; ISR for each interrupt, puts together error, int number and passes it on
+; divide error
 interrupt_handler_00:
     push  0
-    push  0x00
+    push eax
+    mov eax, 0x00
     jmp interrupt_handler
 
+; debug exception
 interrupt_handler_01:
     push  0
-    push  0x01
+    push eax
+    mov eax, 0x01
     jmp interrupt_handler
 
+; nmi interrupt
 interrupt_handler_02:
     push  0
-    push  0x02
+    push eax
+    mov eax, 0x02
     jmp interrupt_handler
 
+; breakpoint
 interrupt_handler_03:
     push  0
-    push  0x03
+    push eax
+    mov eax, 0x03
     jmp interrupt_handler
 
+; overflow
 interrupt_handler_04:
     push  0
-    push  0x04
+    push eax
+    mov eax, 0x04
     jmp interrupt_handler
 
+; bound range
 interrupt_handler_05:
     push  0
-    push  0x05
+    push eax
+    mov eax, 0x05
     jmp interrupt_handler
 
+; invalid opcode
 interrupt_handler_06:
     push  0
-    push  0x06
+    push eax
+    mov eax, 0x06
     jmp interrupt_handler
 
+; no math coprocessor
 interrupt_handler_07:
     push  0
-    push  0x07
+    push eax
+    mov eax, 0x07
     jmp interrupt_handler
 
+; double fault
 interrupt_handler_08:
     ; error info pushed by CPU
-    push  0x08
+    push eax
+    mov eax, 0x08
     jmp interrupt_handler
 
+; coprocessor segment overrun
 interrupt_handler_09:
     push  0
-    push  0x09
+    push eax
+    mov eax, 0x09
     jmp interrupt_handler
 
+; invalid tss
 interrupt_handler_0a:
     ; error info pushed by CPU
-    push  0x0a
+    push eax
+    mov eax, 0x0a
     jmp interrupt_handler
 
+; segment not present
 interrupt_handler_0b:
     ; error info pushed by CPU
-    push  0x0b
+    push eax
+    mov eax, 0x0b
     jmp interrupt_handler
 
+; stack-segment fault
 interrupt_handler_0c:
     ; error info pushed by CPU
-    push  0x0c
+    push eax
+    mov eax, 0x0c
     jmp interrupt_handler
 
+; general protection
 interrupt_handler_0d:
     ; error info pushed by CPU
-    push  0x0d
+    push eax
+    mov eax, 0x0d
     jmp interrupt_handler
 
+; page fault
 interrupt_handler_0e:
     ; error info pushed by CPU
-    push  0x0e
+    push eax
+    mov eax, 0x0e
     jmp interrupt_handler
 
 interrupt_handler_0f:
     push  0
-    push  0x0f
+    push eax
+    mov eax, 0x0f
     jmp interrupt_handler
 
+; fpu fault
 interrupt_handler_10:
     push  0
-    push  0x10
+    push eax
+    mov eax, 0x10
     jmp interrupt_handler
 
+; alignment check
 interrupt_handler_11:
     ; error info pushed by CPU
-    push  0x11
+    push eax
+    mov eax, 0x11
     jmp interrupt_handler
 
+; machine check
 interrupt_handler_12:
     push  0
-    push  0x12
+    push eax
+    mov eax, 0x12
     jmp interrupt_handler
 
+; simd exception
 interrupt_handler_13:
     push  0
-    push  0x13
+    push eax
+    mov eax, 0x13
     jmp interrupt_handler
 
+; virtualization exception
 interrupt_handler_14:
     push  0
-    push  0x14
+    push eax
+    mov eax, 0x14
     jmp interrupt_handler
 
+; control protection exception
 interrupt_handler_15:
     ; error info pushed by CPU
-    push  0x15
+    push eax
+    mov eax, 0x15
     jmp interrupt_handler
 
 interrupt_handler_16:
     push  0
-    push  0x16
+    push eax
+    mov eax, 0x16
     jmp interrupt_handler
 
 interrupt_handler_17:
     push  0
-    push  0x17
+    push eax
+    mov eax, 0x17
     jmp interrupt_handler
 
 interrupt_handler_18:
     push  0
-    push  0x18
+    push eax
+    mov eax, 0x18
     jmp interrupt_handler
 
 interrupt_handler_19:
     push  0
-    push  0x19
+    push eax
+    mov eax, 0x19
     jmp interrupt_handler
 
 interrupt_handler_1a:
     push  0
-    push  0x1a
+    push eax
+    mov eax, 0x1a
     jmp interrupt_handler
 
 interrupt_handler_1b:
     push  0
-    push  0x1b
+    push eax
+    mov eax, 0x1b
     jmp interrupt_handler
 
 interrupt_handler_1c:
     push  0
-    push  0x1c
+    push eax
+    mov eax, 0x1c
     jmp interrupt_handler
 
 interrupt_handler_1d:
     push  0
-    push  0x1d
+    push eax
+    mov eax, 0x1d
     jmp interrupt_handler
 
 interrupt_handler_1e:
     push  0
-    push  0x1e
+    push eax
+    mov eax, 0x1e
     jmp interrupt_handler
 
 interrupt_handler_1f:
     push  0
-    push  0x1f
+    push eax
+    mov eax, 0x1f
     jmp interrupt_handler
 
+; IRQ 0
 interrupt_handler_20:
     push 0
     push eax
     mov eax, 0x20
     jmp interrupt_handler
 
+; IRQ 1
 interrupt_handler_21:
     push 0
     push eax
     mov eax, 0x21
     jmp interrupt_handler
 
+; IRQ 2
 interrupt_handler_22:
     push  0
-    push  0x22
+    push eax
+    mov eax, 0x22
     jmp interrupt_handler
 
+; IRQ 3
 interrupt_handler_23:
     push  0
-    push  0x23
+    push eax
+    mov eax, 0x23
     jmp interrupt_handler
 
+; IRQ 4
 interrupt_handler_24:
     push  0
-    push  0x24
+    push eax
+    mov eax, 0x24
     jmp interrupt_handler
 
+; IRQ 5
 interrupt_handler_25:
     push  0
-    push  0x25
+    push eax
+    mov eax, 0x25
     jmp interrupt_handler
 
+; IRQ 6
 interrupt_handler_26:
     push  0
-    push  0x26
+    push eax
+    mov eax, 0x26
     jmp interrupt_handler
 
+; IRQ 7
 interrupt_handler_27:
     push  0
-    push  0x27
+    push eax
+    mov eax, 0x27
     jmp interrupt_handler
 
+; IRQ 8
 interrupt_handler_28:
     push  0
-    push  0x28
+    push eax
+    mov eax, 0x28
     jmp interrupt_handler
 
+; IRQ 9
 interrupt_handler_29:
     push  0
-    push  0x29
+    push eax
+    mov eax, 0x29
     jmp interrupt_handler
 
+; IRQ a
 interrupt_handler_2a:
     push  0
-    push  0x2a
+    push eax
+    mov eax, 0x2a
     jmp interrupt_handler
 
+; IRQ b
 interrupt_handler_2b:
     push  0
-    push  0x2b
+    push eax
+    mov eax, 0x2b
     jmp interrupt_handler
 
+; IRQ c
 interrupt_handler_2c:
     push  0
-    push  0x2c
+    push eax
+    mov eax, 0x2c
     jmp interrupt_handler
 
+; IRQ d
 interrupt_handler_2d:
     push  0
-    push  0x2d
+    push eax
+    mov eax, 0x2d
     jmp interrupt_handler
 
+; IRQ e
 interrupt_handler_2e:
     push  0
-    push  0x2e
+    push eax
+    mov eax, 0x2e
     jmp interrupt_handler
 
+; IRQ f
 interrupt_handler_2f:
     push  0
-    push  0x2f
+    push eax
+    mov eax, 0x2f
     jmp interrupt_handler
 
+; SYS
 interrupt_handler_30:
     push 0
     push eax
@@ -463,8 +547,10 @@ cli
     jmp .interrupt_handled
 
 .not_sys:
-    ; TODO: Fix this!
     ; Unhandled interrupt
+    push ebx
+    push esi
+    mov ebx, eax
     mov al, TERMINAL_FOREGROUND_RED + TERMINAL_ATTRIB_LIGHT
 
     mov esi, msg_error_unhandled_0 + ADDRESS_KERNEL
@@ -475,12 +561,16 @@ cli
     mov esi, msg_error_unhandled_1 + ADDRESS_KERNEL
     call terminal_print_string
 
-    ;mov ebx, ecx
-    pop ebx
+    mov ebx, [esp + 12]
     call terminal_print_hex
 
     mov esi, msg_error_unhandled_2 + ADDRESS_KERNEL
     call terminal_print_string
+
+    pop esi
+    pop ebx
+    pop eax
+    add esp, 4
     jmp .end
 
 .interrupt_handled:
