5dd66c165454809252fd88f68fbb9305a4b58123
Rafał Grodziński <rafal.grodzinski@gmail.com>
2025-10-17 14:29:18 +0900

    Read disk through bios service

--- boot/bios_service.asm
+++ boot/bios_service.asm
@@ -478,6 +478,17 @@ bios_service:
     call set_video_mode
 .not_set_video_mode:
 
+    ; Read sectors
+    cmp ah, BIOS_SERVICE_READ_SECTORS
+    jne .not_read_sectors
+    push edi ; target address
+    push ecx ; number of sectors
+    push ebx ; lba source address
+    and eax, 0xff
+    push eax ; drive number
+    call service_read_sectors
+.not_read_sectors:
+
     ret
 
 ;
@@ -535,6 +546,59 @@ bits 16
     call switch_to_protected_mode
 bits 32
     ret
+
+;
+; Read sectors from a given device
+; in
+;  drive_number
+;  source_lba_address
+;  sectors_count
+;  target_address
+%define .drive_number [ebp + 8]
+%define .source_lba_address [ebp + 12]
+%define .sectors_count [ebp + 16]
+%define .target_address [ebp + 20]
+bits 32
+service_read_sectors:
+    push ebp
+    mov ebp, esp
+
+    mov ecx, 0 ; sectors counter
+.loop:
+    ; read one sector into a buffer
+    mov eax, .source_lba_address
+    add eax, ecx ; lba address
+    mov ebx, 1 ; read one sector
+    mov edi, buffer 
+    mov dl, .drive_number
+    call read_sectors
+
+    ; copy from buffer into the target addrss
+    mov ebx, 0 ; buffer bytes counter
+.buffer_copy_loop:
+    mov eax, 0x200
+    mul ecx
+    add eax, .target_address
+    add eax, ebx ; calculate target address in eax
+
+    mov dl, [buffer + ebx]
+    mov byte [eax], dl ; finally copy the byte
+
+    inc ebx
+    cmp ebx, 0x200
+    jb .buffer_copy_loop
+
+    inc ecx
+    cmp ecx, .sectors_count
+    jb .loop
+
+    mov esp, ebp
+    pop ebp
+    ret 4 * 4
+%undef .target_address
+%undef .sectors_count
+%undef .source_lba_address
+%undef .drive_number
 
 ;
 ; Stop execution and show error message
