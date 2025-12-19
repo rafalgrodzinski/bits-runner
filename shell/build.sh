04b92eefa2f357c57c7be1ce3ce9e0b9c07f8fcf
Rafał Grodziński <rafal.grodzinski@gmail.com>
2025-12-19 22:37:49 +0900

    Use B instead

--- shell/build.sh
+++ shell/build.sh
@@ -13,7 +13,6 @@ SYS_DIR="${SCRIPT_DIR}/.."
 
 brb --verb=v2 --triple=i686-unknown-linux-gnu --function-sections --no-zero-initialized-in-bss --reloc=static \
 "${SCRIPT_DIR}/main.brc" \
-"${SCRIPT_DIR}/io.brc" \
 "${BRB_DIR}/lib/B/String.brc" \
 "${SYS_DIR}/lib/B/BSys.brc" \
 "${SYS_DIR}/lib/Sys/Syscall.brc" \
@@ -21,7 +20,6 @@ brb --verb=v2 --triple=i686-unknown-linux-gnu --function-sections --no-zero-init
 
 ld.lld -T "${SCRIPT_DIR}/shell.ld" -o shell.bin \
 main.o \
-io.o \
 B.o \
 BSys.o \
 Syscall.o \
