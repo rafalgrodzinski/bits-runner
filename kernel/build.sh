f0917f07ec2ec68f157f8642ee64c8ac4b823b50
Rafał Grodziński <rafal.grodzinski@gmail.com>
2026-01-06 13:41:40 +0900

    Cleaned up folders

--- kernel/build.sh
+++ kernel/build.sh
@@ -13,16 +13,19 @@ function check {
 
 brb --opt=o2 --gen=obj --verb=v2 --function-sections --triple=i686-unknown-linux-gnu --no-zero-initialized-in-bss --reloc=static \
 "${SCRIPT_DIR}/main.brc" \
-"${SCRIPT_DIR}/memory/mem.brc" \
-"${SCRIPT_DIR}/terminal/term.brc" \
-"${SCRIPT_DIR}/drivers/drv_keyboard.brc" \
+\
+"${SCRIPT_DIR}/Mem/Mem.brc" \
+\
+"${SCRIPT_DIR}/Term/Term.brc" \
+\
+"${SCRIPT_DIR}/Drivers/Keyboard/DrvKeyboard.brc" \
 "${SCRIPT_DIR}/BiosService.brc" \
 \
 "${SCRIPT_DIR}/Int/IntHandler.brc" \
 "${SCRIPT_DIR}/Int/SyscallHandler.brc" \
 \
-"${SCRIPT_DIR}/drivers/drv_serial.brc" \
-"${SCRIPT_DIR}/processes/scheduler.brc" \
+"${SCRIPT_DIR}/Sched/Sched.brc" \
+\
 "${SCRIPT_DIR}/Storage/Storage.brc" \
 "${SCRIPT_DIR}/Storage/StorageDevice/StorageDevice.brc" \
 "${SCRIPT_DIR}/Storage/StorageDevice/BiosBootStorageDevice.brc" \
@@ -42,15 +45,14 @@ nasm -f elf32 -o int_raw.o "${SCRIPT_DIR}/Int/int_raw.asm"
 check
 
 ld.lld -T "${SCRIPT_DIR}/kernel.ld" -o kernel.bin \
-term.o \
+Term.o \
 main.o \
-mem.o \
-drv_keyboard.o \
+Mem.o \
+DrvKeyboard.o \
 BiosService.o \
 int_raw.o \
 Int.o \
-drv_serial.o \
-scheduler.o \
+Sched.o \
 Storage.o \
 DrvCmos.o \
 B.o \
