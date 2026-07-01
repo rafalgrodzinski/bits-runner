55c706819dcd75f9fc19ae49bd932efbe740cc1d
Rafał Grodziński <rafal.grodzinski@gmail.com>
2026-07-01 11:38:46 +0900

    Some refactoring

--- bios_service/bios_service.asm
+++ bios_service/bios_service.asm
@@ -27,23 +27,24 @@ bios_service_32:
     sti
 
     ; Reboot
-    cmp ah, BIOS_SERVICE_REBOOT
+    cmp eax, BIOS_SERVICE_REBOOT
     jne .not_reboot
     call bios_service_reboot_32
     jmp .end
 .not_reboot:
 
     ; Video mode
-    cmp ah, BIOS_SERVICE_SET_VIDEO_MODE
+    cmp eax, BIOS_SERVICE_SET_VIDEO_MODE
     jne .not_set_video_mode
+    push ebx ; video_mode
     call bios_service_set_video_mode_32
     jmp .end
 .not_set_video_mode:
 
     ; Read sectors
-    cmp ah, BIOS_SERVICE_BOOT_STORAGE_READ_SECTORS
+    cmp eax, BIOS_SERVICE_BOOT_STORAGE_READ_SECTORS
     jne .not_read_sectors
-    push edi ; target_adr
+    push edx ; target_adr
     push ecx ; sectors_count
     push ebx ; first_sector
     call bios_service_boot_storage_read_sectors_32
@@ -51,9 +52,9 @@ bios_service_32:
 .not_read_sectors:
 
     ; Write sectors
-    cmp ah, BIOS_SERVICE_BOOT_STORAGE_WRITE_SECTORS
+    cmp eax, BIOS_SERVICE_BOOT_STORAGE_WRITE_SECTORS
     jne .not_write_sectors
-    push edi ; source_adr
+    push edx ; source_adr
     push ecx ; sectors_count
     push ebx ; first_sector
     call bios_service_boot_storage_write_sectors_32
@@ -61,7 +62,7 @@ bios_service_32:
 .not_write_sectors:
 
     ; Sectors count
-    cmp ah, BIOS_SERVICE_BOOT_STORAGE_SECTORS_COUNT
+    cmp eax, BIOS_SERVICE_BOOT_STORAGE_SECTORS_COUNT
     jne .not_sectors_count
     call bios_service_boot_storage_sectors_count_32
     jmp .end
@@ -88,14 +89,18 @@ bios_service_reboot_32:
 ;
 ; Change video mode
 ; in
-;  al: video mode
+;  video_mode
+%define .args_count 1
+%define .video_mode [ebp + 8]
 [bits 32]
 bios_service_set_video_mode_32:
-    call switch_to_v86_mode_32
+    push ebp
+    mov ebp, esp
 
+    call switch_to_v86_mode_32
 [bits 16]
     ; text 80x25
-    cmp al, BIOS_SERVICE_TEXT_MODE_80x25
+    cmp dword .video_mode, BIOS_SERVICE_TEXT_MODE_80x25
     jne .not_80x25
     mov ax, 0x0003
     mov bl, 0
@@ -104,7 +109,7 @@ bios_service_set_video_mode_32:
 .not_80x25:
 
     ; text 80x50
-    cmp al, BIOS_SERVICE_TEXT_MODE_80x50
+    cmp dword .video_mode, BIOS_SERVICE_TEXT_MODE_80x50
     jne .not_80x50
     mov ax, 0x1112
     mov bl, 0
@@ -113,7 +118,7 @@ bios_service_set_video_mode_32:
 .not_80x50:
 
     ;graphics 320x200x8
-    cmp al, BIOS_SERVICE_GPXS_MODE_320x200x8
+    cmp dword .video_mode, BIOS_SERVICE_GPXS_MODE_320x200x8
     jne .not_320x200x8
     mov ax, 0x0013
     int 0x10
@@ -121,7 +126,7 @@ bios_service_set_video_mode_32:
 .not_320x200x8:
 
     ;graphics 640x480x4
-    cmp al, BIOS_SERVICE_GPXS_MODE_640x480x4
+    cmp dword .video_mode, BIOS_SERVICE_GPXS_MODE_640x480x4
     jne .not_640x480x4
     mov ax, 0x0012
     int 0x10
@@ -131,7 +136,12 @@ bios_service_set_video_mode_32:
 .end:
     call switch_to_protected_mode_16
 [bits 32]
-    ret
+
+    mov esp, ebp
+    pop ebp
+    ret U32_SIZE * .args_count
+%undef .video_mode
+%undef .args_count
 
 ;
 ; Read sectors from the boot storage
